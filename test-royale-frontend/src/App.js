78723169f4ef0df1b9551bfdc978803ce13372cc
ferencDobi <ferencdbi@gmail.com>
2018-11-24 20:52:00 +0100

    game page add

--- test-royale-frontend/src/App.js
+++ test-royale-frontend/src/App.js
@@ -4,6 +4,7 @@ import {MainPage} from './components/MainPage';
 import './sass/App.scss';
 import {Game} from "./components/Game";
 import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
                 <Header />
                 <Route exact path="/" component={MainPage} />
                 <Route path="/game/" component={Game} />
+                <Footer />
             </Fragment>
         </Router>
     );
